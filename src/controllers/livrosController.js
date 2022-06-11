722b90b3dfb63ae8e75b2272b4922ac5945ef220
Rodrigo Gomes <cklrod@hotmail.com>
2022-06-11 19:28:57 -0300

    adicionando novo schema, refrenciando atributos

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,7 +3,7 @@ import livros from "../model/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
+        livros.find().populate('autor').exec((err, livros) => {
             res.status(200).json(livros);
         })
     }
@@ -11,12 +11,20 @@ class LivroController {
     static listarLivrosPorId = (req, res) =>{
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) =>{
+        livros.findById(id).populate('autor', 'nome').exec((err, livros) =>{
             if(err){
                 res.status(400).send({message: `${err.message} - id do livro não encontrado.`});
             }else{
                 res.status(200).json(livros);
             }
+        })
+    }
+
+    static listarEditora = (req, res) =>{
+        const editora = req.query.editora;
+
+        livros.find({'editora': editora}, {},(err, livros) => {
+            res.status(200).send(livros);
         })
     }
 
