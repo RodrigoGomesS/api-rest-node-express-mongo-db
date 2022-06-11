ecb04604a76595e66132406fcff257b5740a8134
Rodrigo Gomes <cklrod@hotmail.com>
2022-06-11 19:28:57 -0300

    adicionando novo schema, refrenciando atributos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
-import app from "../app.js";
-
+import autores from "./autoresRoutes.js";
 const routes = (app) => {
     app.route('/').get((req, res) => {
         res.status(200).send({ ttitulo: 'Curso de Node'});
@@ -9,7 +8,8 @@ const routes = (app) => {
 
     app.use(
         express.json(),
-        livros
+        livros,
+        autores
     );
 }
 
